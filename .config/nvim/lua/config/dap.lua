a90acc65e70379d998f24bf29b4c1aecd7ed4a31
Lucas Vieira <lucasscvvieira@gmail.com>
2021-12-15 09:02:49 -0300

    refactor(nvim): fix python debug execution

--- .config/nvim/lua/config/dap.lua
+++ .config/nvim/lua/config/dap.lua
@@ -4,7 +4,7 @@ function M.config()
   local dap = require("dap")
   dap.adapters.python = {
     type = "executable";
-    command = "/usr/local/bin/python";
+    command = "python3";
     args = { "-m", "debugpy.adapter" };
   }
 
