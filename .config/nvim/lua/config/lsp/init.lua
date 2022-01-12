11c8887a123ca7e19e311e16baa7fe05492bf628
Lucas Vieira <lucasscvvieira@gmail.com>
2022-01-12 11:31:57 -0300

    refactor(nvim): update lsp python config

--- .config/nvim/lua/config/lsp/init.lua
+++ .config/nvim/lua/config/lsp/init.lua
@@ -49,6 +49,14 @@ function M.config()
       settings = {
         pylsp = {
           configurationSources = {"flake8"},
+          plugins = {
+            flake8 = {
+              enabled = true,
+            },
+            pycodestyle = {
+              enabled = false,
+            },
+          },
         },
       },
     },
