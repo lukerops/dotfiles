4a3f9829c1a7411a7efc7e1917fdea632ce992c4
Lucas Campos Vieira <lucasscvvieira@gmail.com>
2022-03-04 18:29:19 -0300

    refactor(nvim): enable lsp update in insert

--- .config/nvim/lua/config/lsp/init.lua
+++ .config/nvim/lua/config/lsp/init.lua
@@ -14,7 +14,7 @@ function M.config()
       virtual_text = true,
       signs = true,
       -- Disable a feature
-      update_in_insert = false,
+      update_in_insert = true,
   })
   
   local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
