8935bc107471b8c386cd3d3e8f4dc2c0f56a7977
Lucas Campos Vieira <lucasscvvieira@gmail.com>
2022-06-14 10:19:44 -0300

    refactor(nvim): format file

--- .config/nvim/lua/config/ultest.lua
+++ .config/nvim/lua/config/ultest.lua
@@ -36,29 +36,29 @@ end
 
 function M.config()
   local builders = {
-		python = function(cmd)
-			local non_modules = { "python", "pipenv", "poetry" }
+	python = function(cmd)
+      local non_modules = { "python", "pipenv", "poetry" }
 
-			local module_index
-			if vim.tbl_contains(non_modules, cmd[1]) then
-				module_index = 3
-			else
-				module_index = 1
-			end
+      local module_index
+      if vim.tbl_contains(non_modules, cmd[1]) then
+        module_index = 3
+      else
+        module_index = 1
+      end
 
-			local args = vim.list_slice(cmd, module_index + 1)
+      local args = vim.list_slice(cmd, module_index + 1)
 
-			return {
-				dap = {
-					type = "python",
-					name = "Ultest Debugger",
-					request = "launch",
-					module = cmd[module_index],
-					args = args,
-					justMyCode = false,
-				},
-			}
-		end,
+      return {
+        dap = {
+          type = "python",
+          name = "Ultest Debugger",
+          request = "launch",
+          module = cmd[module_index],
+          args = args,
+          justMyCode = false,
+        },
+      }
+    end,
   }
 
   require("ultest").setup({ builders = builders })
