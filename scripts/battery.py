4f7647dcbfabfd10ea5fdd0895c05281ad64521a
Lucas Campos Vieira <contato@lukerops.com>
2023-08-29 12:02:39 -0300

    scripts: adiciona script para monitoria da bateria

---
+++ scripts/battery.py
@@ -0,0 +1,58 @@
+import csv
+import os
+from datetime import date
+
+USER = os.environ['USER']
+HOME = f'/home/{USER}/'
+FILEPATH = f'{HOME}/battery.csv'
+FIELDNAMES = ['date', 'capacity']
+
+
+def measure():
+    with open('/sys/class/power_supply/BAT0/charge_full', 'r') as file:
+        full = float(file.readline())
+
+    with open('/sys/class/power_supply/BAT0/charge_full_design', 'r') as file:
+        design = float(file.readline())
+
+    return {
+        'date': date.today(),
+        'capacity': 100 * full / design,
+    }
+
+
+def load():
+    try:
+        with open(FILEPATH, 'r+') as csvfile:
+            csv_reader = csv.DictReader(csvfile)
+            rows = [
+                {
+                    'date': date.fromisoformat(row['date']),
+                    'capacity': float(row['capacity']),
+                }
+                for row in csv_reader
+            ]
+    except FileNotFoundError:
+        rows = list()
+
+    return rows
+
+
+def save(rows):
+    with open(FILEPATH, 'w+') as csvfile:
+        csv_writer = csv.DictWriter(csvfile, fieldnames=FIELDNAMES)
+        csv_writer.writeheader()
+        csv_writer.writerows(rows)
+
+
+def main():
+    rows = load()
+    if date.today() in [row['date'] for row in rows]:
+        return
+
+    rows.append(measure())
+    save(rows)
+
+
+if __name__ == '__main__':
+    main()
