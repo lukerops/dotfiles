e986f5c67b1f47724bb878b6e0530193dc1e1177
Lucas Campos Vieira <contato@lukerops.com>
2024-03-06 17:21:57 -0300

    nvim: atualiza código do pylsp global

--- nvim/lua/python/init.lua
+++ nvim/lua/python/init.lua
@@ -1,34 +1,74 @@
 local M = {}
 
 local function uses_pyenv(workspace)
-  local match = vim.fn.glob(vim.fs.normalize(workspace .. '/.python-version'))
-  return match ~= ''
+  local paths = vim.fs.find('.python-version', {
+    upward = false,
+    type = 'file',
+    path = workspace,
+  })
+
+  return vim.tbl_count(paths) > 0
 end
 
 local function get_pyenv_python_path(version)
-  local match = vim.fn.glob('~/.pyenv/versions/' .. version .. '*')
-  if match == '' then
+  local versions = {}
+  local nums = vim.split(version, '.', { plain = true })
+
+  for i = 1, vim.tbl_count(nums) do
+    local _version = {}
+    for j = 1, i do
+      table.insert(_version, nums[j])
+    end
+
+    table.insert(versions, vim.fn.join(_version, '.'))
+  end
+
+  local paths = vim.fs.find(versions, {
+    upward = true,
+    limit = math.huge,
+    type = 'directory',
+    path = vim.fs.normalize('~/.pyenv/versions/'),
+    stop = vim.fs.normalize('~/.pyenv/'),
+  })
+
+  if vim.tbl_count(paths) == 0 then
     vim.notify('Python ' .. version .. ' not installed by pyenv.', vim.log.levels.ERROR)
     return
   end
 
-  local versions = vim.split(match, '\n')
-  table.sort(versions)
-
-  return vim.fs.normalize(versions[1] .. '/bin/python3')
+  table.sort(paths)
+  return vim.fs.normalize(paths[1] .. '/bin/python3')
 end
 
 local function get_python_path(workspace)
   local python = 'python3'
+
   if uses_pyenv(workspace) then
-    python = get_pyenv_python_path(vim.fn.readfile(workspace .. '/.python-version')[1])
+    local pyenv_python = get_pyenv_python_path(vim.fn.readfile(workspace .. '/.python-version')[1])
+    if pyenv_python then
+      python = pyenv_python
+    end
   end
 
-  -- check if the project uses poetry
-  local match = vim.fn.glob(vim.fs.normalize(workspace .. '/poetry.lock'))
-  if match ~= '' then
+  -- uses poetry's venv python
+  local paths = vim.fs.find('poetry.lock', {
+    upward = false,
+    type = 'file',
+    path = workspace,
+  })
+  if vim.tbl_count(paths) > 0 then
     local venv = vim.fn.trim(vim.fn.system({ python, '-m', 'poetry', '--directory', workspace, 'env', 'info', '-p' }))
     return vim.fs.normalize(venv .. '/bin/python3')
+  end
+
+  -- uses the venv python
+  paths = vim.fs.find({ 'venv', '.venv' }, {
+    upward = false,
+    type = 'directory',
+    path = workspace,
+  })
+  if vim.tbl_count(paths) > 0 then
+    return vim.fs.normalize(paths[1] .. '/bin/python3')
   end
 
   return python
