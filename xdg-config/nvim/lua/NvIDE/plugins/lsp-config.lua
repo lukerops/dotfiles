56fdf4d64769cfaee308142a1cdd1eae1353ae53
Lucas Campos Vieira <contato@lukerops.com>
2024-05-16 16:21:11 -0300

    Revert "nvim: para de utilizar o mason para lsp de python"
    This reverts commit bce1c1fc146ea409fad80c810e8c35affa2bcdf3.

--- xdg-config/nvim/lua/NvIDE/plugins/lsp-config.lua
+++ xdg-config/nvim/lua/NvIDE/plugins/lsp-config.lua
@@ -34,7 +34,7 @@ return {
     -- configura alguns LSPs com a configuração padrão
     -- lsp_zero.setup_servers({ "pylsp" })
     lsp_zero.configure("pylsp", {
-      -- on_init = require('python').on_init,
+      on_init = require('python').on_init,
       settings = {
         pylsp = {
           plugins = {
@@ -50,7 +50,7 @@ return {
     })
 
     require("mason-lspconfig").setup({
-      ensure_installed = { "lua_ls", "gopls", "terraformls" },
+      ensure_installed = { "lua_ls", "pylsp", "gopls", "terraformls" },
       handlers = {
         lsp_zero.default_setup,
       }
