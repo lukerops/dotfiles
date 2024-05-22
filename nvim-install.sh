4ba1b8ab8d1f698de285db893c5f599f7d020804
Lucas Campos Vieira <contato@lukerops.com>
2024-05-22 17:13:18 -0300

    nvim: atualiza script de instalação

--- nvim-install.sh
+++ nvim-install.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+BASE_DIR="$HOME/.var/app/io.neovim.nvim"
+
 function extension() {
     echo $(flatpak info --show-sdk io.neovim.nvim | sed "s/Sdk/Sdk.Extension.$1/")
+}
+
+function install_pyenv() {
+    git clone https://github.com/pyenv/pyenv.git $BASE_DIR/.pyenv
+    cat <<'EOF' >> $BASE_DIR/.bashrc
+
+# pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+EOF
 }
 
 # instala o Neovim via flatpak
@@ -9,26 +22,71 @@ flatpak install -y flathub io.neovim.nvim
 
 # instala as extensões
 flatpak install -y $(extension golang)
+flatpak install -y $(extension node20)
+flatpak install -y $(extension rust-stable)
 
 # modifica algumas configurações
 flatpak --user override \
-    --socket=gpg-agent \
     --socket=ssh-auth \
-    --env=SHELL=/usr/bin/bash \
-    --env=FLATPAK_ENABLE_SDK_EXT=golang \
+    --env='SHELL=/usr/bin/bash' \
     --nofilesystem=host \
     --nofilesystem=/tmp \
     --nofilesystem=/var/tmp \
     --filesystem=~/git \
     --filesystem=~/.gitconfig \
+    --filesystem=~/.ssh \
     --persist=.bashrc \
     --persist=.bash_history \
-    --persist=.ssh \
     --persist=.pyenv \
+    --persist=go \
+    --persist=.start.sh \
         io.neovim.nvim
 
+# adiciona os arquivos do bash
+[ ! -f $BASE_DIR/.bashrc ] && touch $BASE_DIR/.bashrc
+[ ! -f $BASE_DIR/.bash_history ] && touch $BASE_DIR/.bash_history
+
 # cria as pastas que vamos copiar os arquivos
-mkdir -p ~/.var/app/io.neovim.nvim/config/ ~/.var/app/io.neovim.nvim/data/nvim/mason/bin/
+mkdir -p \
+    $BASE_DIR/config/ \
+    $BASE_DIR/data/nvim/mason/bin/ \
+    $BASE_DIR/go
 
 # adiciona a config do nvim
-[ ! -L $HOME/.var/app/io.neovim.nvim/config//nvim ] && ln -s $(pwd)/xdg-config/nvim $HOME/.var/app/io.neovim.nvim/config/
+[ ! -L $BASE_DIR/config/nvim ] && ln -s $(pwd)/xdg-config/nvim $BASE_DIR/config/
+
+# instala o pyenv
+[ ! -d $BASE_DIR/.pyenv ] && install_pyenv
+
+# cria script de inicialização
+[ ! -f $BASE_DIR/.start.sh ] && touch $BASE_DIR/.start.sh && chmod +x $BASE_DIR/.start.sh
+cat <<'EOF' > $BASE_DIR/.start.sh
+#!/usr/bin/bash
+
+# obtém a lista de extensões instaladas
+SDK=()
+for d in /usr/lib/sdk/*; do
+    SDK+=("${d##*/}")
+done
+
+# habilita as extensões instaladas
+for i in "${SDK[@]}"; do
+    sdk_ext_dir="/usr/lib/sdk/$i"
+    if [[ -d "$sdk_ext_dir" && -f "$sdk_ext_dir/enable.sh" ]]; then
+        . "$sdk_ext_dir/enable.sh"
+    fi
+done
+
+# usa os pacotes instalados pelo golang
+PATH="$HOME/go/bin:$PATH"
+[ -z $(which lazygit) ] && go install github.com/jesseduffield/lazygit@latest
+
+PATH="$XDG_DATA_HOME/cargo/bin:$PATH"
+[ -z $(which rg) ] && cargo install ripgrep
+
+# inicia o nvim
+/app/bin/nvim-wrapper
+EOF
+
+echo 'Adicione o atalho de inicialização:'
+echo '  alias nvim="flatpak run --command=$HOME/.start.sh io.neovim.nvim"'
