aead18f4bd260455b77c41700c002e8bf47468e8
Lucas Campos Vieira <contato@lukerops.com>
2024-07-08 19:53:06 -0300

    nvim: ajusta config do statuscol

--- xdg-config/nvim/lua/NvIDE/plugins/statuscol.lua
+++ xdg-config/nvim/lua/NvIDE/plugins/statuscol.lua
@@ -4,12 +4,12 @@ return {
     local builtin = require("statuscol.builtin")
     require("statuscol").setup({
       segments = {
+        { sign = { namespace = { "gitsign", "diagnostic" }, colwidth = 1, wrap = true },    click = "v:lua.ScSa" },
+        -- { sign = { namespace = { "diagnostic" }, maxwidth = 1 },                            click = "v:lua.ScSa" },
         { text = { builtin.foldfunc },                                                      click = "v:lua.ScFa" },
         { sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true, wrap = true }, click = "v:lua.ScSa" },
-        { sign = { namespace = { "diagnostic" }, maxwidth = 1 },                            click = "v:lua.ScSa" },
-        { text = { builtin.lnumfunc },                                                      click = "v:lua.ScLa" },
         { text = { " " } },
-        { sign = { namespace = { "gitsign" }, colwidth = 1, wrap = true },                  click = "v:lua.ScSa" },
+        { text = { builtin.lnumfunc },                                                      click = "v:lua.ScLa" },
         { text = { " " } },
       }
     })
