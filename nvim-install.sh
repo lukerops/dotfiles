508caafc918e309e539f21c5edf2a07787d56a79
Lucas Campos Vieira <contato@lukerops.com>
2024-08-13 17:59:23 -0300

    nvim: adiciona instalação fora do flatpak

--- nvim-install.sh
+++ nvim-install.sh
@@ -20,8 +20,10 @@ function extension() {
 }
 
 function install_pyenv() {
-    git clone https://github.com/pyenv/pyenv.git $BASE_DIR/.pyenv
-    cat <<'EOF' >> $BASE_DIR/.bashrc
+    local home=${1:-$HOME}
+
+    git clone https://github.com/pyenv/pyenv.git $home/.pyenv
+    cat <<'EOF' >> $home/.bashrc
 
 # pyenv
 export PYENV_ROOT="$HOME/.pyenv"
@@ -31,58 +33,94 @@ EOF
 }
 
 function install_tfenv() {
-    git clone --depth=1 https://github.com/tfutils/tfenv.git $BASE_DIR/.tfenv
-    echo 'export PATH=$PATH:$HOME/.tfenv/bin' >> $BASE_DIR/.bashrc
+    local home=${1:-$HOME}
+    
+    git clone --depth=1 https://github.com/tfutils/tfenv.git $home/.tfenv
+    echo 'export PATH=$HOME/.tfenv/bin:$PATH' >> $home/.bashrc
 }
 
-# instala o Neovim via flatpak
-flatpak install -y flathub io.neovim.nvim
+function install_neovim() {
+    cwd=$(pwd)
+    tmpdir=$(mktemp -d)
+    cd $tmpdir
 
-# instala as extensões
-flatpak install -y $(extension golang)
-flatpak install -y $(extension node20)
-flatpak install -y $(extension rust-stable)
+    wget 'https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz'
+    wget 'https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz.sha256sum'
 
-# modifica algumas configurações
-flatpak --user override \
-    --socket=ssh-auth \
-    --env='SHELL=/usr/bin/bash' \
-    --nofilesystem=host \
-    --nofilesystem=/tmp \
-    --nofilesystem=/var/tmp \
-    --filesystem=~/git \
-    --filesystem=~/.gitconfig \
-    --filesystem=~/.ssh \
-    --persist=.bashrc \
-    --persist=.bash_history \
-    --persist=.pyenv \
-    --persist=.tfenv \
-    --persist=go \
-    --persist=.start.sh \
-        io.neovim.nvim
+    if [[ -z $(sha256sum -c "$tmpdir/nvim-linux64.tar.gz.sha256sum" 2>&1 | egrep 'OK|SUCESSO') ]]; then
+        echo 'Neovim SHA256 com falha.'
+        exit 1
+    fi
 
-# adiciona os arquivos do bash
-[ ! -f $BASE_DIR/.bashrc ] && touch $BASE_DIR/.bashrc
-[ ! -f $BASE_DIR/.bash_history ] && touch $BASE_DIR/.bash_history
+    sudo rm -rf /opt/nvim
+    sudo mkdir -p /opt/nvim
+    sudo tar --directory=/opt/nvim --file=nvim-linux64.tar.gz --extract --gzip --strip-components=1 nvim-linux64
 
-# cria as pastas que vamos copiar os arquivos
-mkdir -p \
-    $BASE_DIR/config/ \
-    $BASE_DIR/data/nvim/mason/bin/ \
-    $BASE_DIR/go
+    [[ -z $(cat $HOME/.bashrc | grep '/opt/nvim/bin') ]] && echo 'export PATH=/opt/nvim/bin:$PATH' >> $HOME/.bashrc
 
-# adiciona a config do nvim
-[ ! -L $BASE_DIR/config/nvim ] && ln -s $(pwd)/xdg-config/nvim $BASE_DIR/config/
+    cd $cwd
+    rm -rf $tmpdir
 
-# instala o pyenv
-[ ! -d $BASE_DIR/.pyenv ] && install_pyenv
+    # adiciona a config do nvim
+    [ ! -L $HOME/config/nvim ] && ln -s $(pwd)/xdg-config/nvim $HOME/.config/
 
-# instala o tfenv
-[ ! -d $BASE_DIR/.tfenv ] && install_tfenv
+    # instala o pyenv
+    [ ! -d $HOME/.pyenv ] && install_pyenv
 
-# cria script de inicialização
-[ ! -f $BASE_DIR/.start.sh ] && touch $BASE_DIR/.start.sh && chmod +x $BASE_DIR/.start.sh
-cat <<'EOF' > $BASE_DIR/.start.sh
+    # instala o tfenv
+    [ ! -d $HOME/.tfenv ] && install_tfenv
+}
+
+function install_neovim_flatpak() {
+
+    # instala o Neovim via flatpak
+    flatpak install -y flathub io.neovim.nvim
+
+    # instala as extensões
+    flatpak install -y $(extension golang)
+    flatpak install -y $(extension node20)
+    flatpak install -y $(extension rust-stable)
+
+    # modifica algumas configurações
+    flatpak --user override \
+        --socket=ssh-auth \
+        --env='SHELL=/usr/bin/bash' \
+        --nofilesystem=host \
+        --nofilesystem=/tmp \
+        --nofilesystem=/var/tmp \
+        --filesystem=~/git \
+        --filesystem=~/.gitconfig \
+        --filesystem=~/.ssh \
+        --persist=.bashrc \
+        --persist=.bash_history \
+        --persist=.pyenv \
+        --persist=.tfenv \
+        --persist=go \
+        --persist=.start.sh \
+            io.neovim.nvim
+
+    # adiciona os arquivos do bash
+    [ ! -f $BASE_DIR/.bashrc ] && touch $BASE_DIR/.bashrc
+    [ ! -f $BASE_DIR/.bash_history ] && touch $BASE_DIR/.bash_history
+
+    # cria as pastas que vamos copiar os arquivos
+    mkdir -p \
+        $BASE_DIR/config/ \
+        $BASE_DIR/data/nvim/mason/bin/ \
+        $BASE_DIR/go
+
+    # adiciona a config do nvim
+    [ ! -L $BASE_DIR/config/nvim ] && ln -s $(pwd)/xdg-config/nvim $BASE_DIR/config/
+
+    # instala o pyenv
+    [ ! -d $BASE_DIR/.pyenv ] && install_pyenv $BASE_DIR
+
+    # instala o tfenv
+    [ ! -d $BASE_DIR/.tfenv ] && install_tfenv $BASE_DIR
+
+    # cria script de inicialização
+    [ ! -f $BASE_DIR/.start.sh ] && touch $BASE_DIR/.start.sh && chmod +x $BASE_DIR/.start.sh
+    cat <<'EOF' > $BASE_DIR/.start.sh
 #!/usr/bin/bash
 
 # obtém a lista de extensões instaladas
@@ -110,7 +148,10 @@ PATH="$XDG_DATA_HOME/cargo/bin:$PATH"
 /app/bin/nvim-wrapper
 EOF
 
-install_fonts
+    install_fonts
 
-echo 'Adicione o atalho de inicialização:'
-echo '  alias nvim="flatpak run --command=$HOME/.start.sh io.neovim.nvim"'
+    echo 'Adicione o atalho de inicialização:'
+    echo '  alias nvim="flatpak run --command=$HOME/.start.sh io.neovim.nvim"'
+}
+
+install_neovim
