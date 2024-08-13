db1a04bec54242bcace24ff4f249c91394a72db4
Lucas Campos Vieira <contato@lukerops.com>
2024-08-13 18:05:13 -0300

    nvim: configura as env vars no neotest

--- xdg-config/nvim/lua/NvIDE/plugins/neotest.lua
+++ xdg-config/nvim/lua/NvIDE/plugins/neotest.lua
@@ -11,48 +11,47 @@ return {
   keys = {
     {
       "<leader>tf",
-      function()
-        require("neotest").run.run({ vim.fn.expand("%"), env = require("dotenv").get_envs() })
-      end,
+      function() require("neotest").run.run({ vim.fn.expand("%") }) end,
       desc = "Run All Tests in the file",
     },
     {
       "<leader>tn",
-      function()
-        require("neotest").run.run({ env = require("dotenv").get_envs() })
-      end,
+      function() require("neotest").run.run() end,
       desc = "Run the nearest test",
     },
     {
       "<leader>to",
-      function()
-        require("neotest").output.open({ enter = true })
-      end,
+      function() require("neotest").output.open({ enter = true }) end,
       desc = "Open the result of the nearest test",
     },
     {
       "<leader>ts",
-      function()
-        require("neotest").summary.toggle()
-      end,
+      function() require("neotest").summary.toggle() end,
       desc = "Open the Test Summary",
     },
     {
       "<leader>tdbg",
-      function()
-        require("neotest").run.run({ strategy = "dap", env = require("dotenv").get_envs() })
-      end,
+      function() require("neotest").run.run({ strategy = "dap" }) end,
       desc = "Run the nearest test in debug",
     },
   },
-  config = function()
-    require("neotest").setup({
+  opts = function()
+    return {
+      run = {
+        augment = function(_, args)
+          -- Add the environment variables from the .env file
+          args.env = vim.tbl_deep_extend("force", args.env or {}, require("dotenv").get_envs())
+
+          return args
+        end,
+      },
       adapters = {
         require("neotest-python")({
+          args = { "-vv" },
           runner = "pytest",
           python = require("python").get_python_path(vim.loop.cwd()),
         }),
       }
-    })
+    }
   end,
 }
