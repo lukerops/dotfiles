fda2e2b992cf37a4a2e69f2ed722c4af0cce5fe7
Lucas Campos Vieira <contato@lukerops.com>
2024-09-13 09:49:32 -0300

    distro: adiciona script de instalação do neovim

---
+++ distro/apps/nvim.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+# cria uma pasta temporária
+tmp=$(mktemp -d)
+
+# Faz o donwload da versão estável mais recente do neovim
+wget --directory-prefix $tmp --relative 'https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz'
+
+# Remove versão anterior do neovim
+sudo rm -rf /opt/nvim
+
+# Extraí o arquivo baixado na pasta /opt
+sudo tar --extract --gzip --directory=/opt --file=$tmp/nvim-linux64.tar.gz
+
+# Renomeia a pasta para /opt/nvim
+sudo mv /opt/nvim-linux64 /opt/nvim
+
+# Cria um link simbólico para o executável
+[[ ! -L /usr/local/bin/nvim ]] && sudo ln -s /opt/nvim/bin/nvim /usr/local/bin/
+
+# Remove a pasta temporária
+rm -rf $tmp
