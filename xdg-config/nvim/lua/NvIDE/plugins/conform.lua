840520996ea11d8ef0e83b529696376279b7038b
Lucas Campos Vieira <contato@lukerops.com>
2024-09-19 09:36:19 -0300

    nvim: remove o autoformat para alguns arquivos

--- xdg-config/nvim/lua/NvIDE/plugins/conform.lua
+++ xdg-config/nvim/lua/NvIDE/plugins/conform.lua
@@ -5,10 +5,18 @@ return {
       python = { 'ruff_format', 'ruff_organize_imports' },
       terraform = { 'terraform_fmt' },
     },
-    format_on_save = {
-      -- I recommend these options. See :help conform.format for details.
-      lsp_fallback = true,
-      timeout_ms = 500,
-    }
+    format_on_save = function(bufnr)
+      -- Disable autoformat on certain filetypes
+      local ignore_filetypes = { "sql" }
+      if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+        return
+      end
+
+      return {
+        -- I recommend these options. See :help conform.format for details.
+        lsp_fallback = true,
+        timeout_ms = 500,
+      }
+    end,
   },
 }
