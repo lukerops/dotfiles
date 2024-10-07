cb0d9cd1b05d2797ebfaf14f36dc749588fbbcc4
Lucas Campos Vieira <contato@lukerops.com>
2024-10-07 14:39:19 -0300

    nvim: remove plugins (neorg, octo, repolink)

--- xdg-config/nvim/lua/NvIDE/plugins/octo.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "pwntester/octo.nvim",
-  cmd = { "Octo", },
-  dependencies = { "nvim-telescope/telescope.nvim" },
-  config = true,
-}
--- xdg-config/nvim/lua/NvIDE/plugins/repolink.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "9seconds/repolink.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  cmd = "RepoLink",
-  keys = {
-    { "<leader>gl", "<cmd>RepoLink<cr>", desc = "Generate Git HTTP permalink" },
-  },
-  config = true,
-}
