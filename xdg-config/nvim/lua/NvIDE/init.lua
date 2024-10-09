eef165c5f83aa1dca5c4150b590836293586d7a9
Lucas Campos Vieira <contato@lukerops.com>
2024-10-09 10:21:27 -0300

    nvim: ajusta algumas config e keymaps

--- xdg-config/nvim/lua/NvIDE/init.lua
+++ xdg-config/nvim/lua/NvIDE/init.lua
@@ -4,24 +4,24 @@ require("NvIDE.keymaps")
 -- global statusbar
 vim.opt.laststatus = 3
 
--- set tab width
 vim.opt.tabstop = 4
-vim.opt.softtabstop = 0
-vim.opt.expandtab = true
+vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+
+vim.opt.expandtab = true
 vim.opt.smarttab = true
+vim.opt.smartindent = true
 
 vim.opt.hidden = true
 vim.opt.title = true
 
--- global statusbar
-vim.opt.laststatus = 3
+vim.opt.mouse = "a"
+vim.opt.clipboard = "unnamedplus"
 
 vim.opt.termguicolors = true
 vim.opt.ignorecase = true
 vim.opt.encoding = "utf-8"
-vim.opt.clipboard = "unnamedplus"
-vim.opt.mouse = "a"
+
 vim.opt.number = true
 vim.opt.cursorline = true
 vim.opt.signcolumn = "yes"
@@ -31,12 +31,5 @@ vim.api.nvim_create_autocmd({ "BufEnter" }, {
   pattern = { "*.tf", "*.lua" },
   callback = function()
     vim.bo.shiftwidth = 2
-  end,
-})
-
-vim.api.nvim_create_autocmd({ "BufEnter" }, {
-  pattern = { "*.norg" },
-  callback = function()
-    vim.opt.conceallevel = 2
   end,
 })
--- xdg-config/nvim/lua/NvIDE/keymaps.lua
+++ xdg-config/nvim/lua/NvIDE/keymaps.lua
@@ -1,9 +1,6 @@
-vim.keymap.set("n", "<M-Up>", ":m .-2<CR>==", { noremap = true })
-vim.keymap.set("n", "<M-Down>", ":m .+1<CR>==", { noremap = true })
-vim.keymap.set("i", "<M-Up>", "<Esc>:m .-2<CR>==gi", { noremap = true })
-vim.keymap.set("i", "<M-Down>", "<Esc>:m .+1<CR>==gi", { noremap = true })
-vim.keymap.set("v", "<M-Up>", ":m '<-2<CR>==gv", { noremap = true })
-vim.keymap.set("v", "<M-Down>", ":m '>+1<CR>==gv", { noremap = true })
-
--- Adiciona suporte ao Lazygit
-vim.keymap.set("n", "<leader>go", function() require("lazy.util").float_term("lazygit") end, { desc = "Open Lazygit" })
+vim.keymap.set("n", "<M-Up>", ":m .-2<CR>==")
+vim.keymap.set("n", "<M-Down>", ":m .+1<CR>==")
+vim.keymap.set("i", "<M-Up>", "<Esc>:m .-2<CR>==gi")
+vim.keymap.set("i", "<M-Down>", "<Esc>:m .+1<CR>==gi")
+vim.keymap.set("v", "<M-Up>", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "<M-Down>", ":m '>+1<CR>gv=gv")
