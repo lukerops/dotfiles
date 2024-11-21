bdcb1644ffd0f2a2ee4db3ec0f833186d47b6d95
Lucas Campos Vieira <contato@lukerops.com>
2024-11-21 15:44:51 -0300

    nvim: ajusta o clipboard no ssh

--- xdg-config/nvim/lua/NvIDE/init.lua
+++ xdg-config/nvim/lua/NvIDE/init.lua
@@ -17,6 +17,18 @@ vim.opt.title = true
 
 vim.opt.mouse = "a"
 vim.opt.clipboard = "unnamedplus"
+-- :h clipboard-osc52
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
 
 vim.opt.termguicolors = true
 vim.opt.ignorecase = true
