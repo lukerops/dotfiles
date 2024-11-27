a9a15613cdddffed06f0b41d66c71b93a4cd9463
Lucas Campos Vieira <contato@lukerops.com>
2024-11-27 11:50:36 -0300

    distro: instala as fontes JetBrainsMono e ZedMono

--- distro/common.sh
+++ distro/common.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/bash
 
+# Instala as fontes customizadas
+TMPFILE=$(mktemp)
+if [[ ! -d "$HOME/.local/share/fonts/JetBrainsMono" ]]; then
+    wget -O $TMPFILE 'https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip'
+    unzip -d "$HOME/.local/share/fonts/JetBrainsMono" $TMPFILE
+fi
+
+if [[ ! -d "$HOME/.local/share/fonts/ZedMono" ]]; then
+    wget -O $TMPFILE 'https://github.com/ryanoasis/nerd-fonts/releases/latest/download/ZedMono.zip'
+    unzip -d "$HOME/.local/share/fonts/ZedMono" $TMPFILE
+fi
+
+rm $TMPFILE
+
 if [[ $DESKTOP == 'gnome' ]]; then
     flatpak install flathub $(cat ./distro/pkgs/gnome.desktop.flatpak.pkgs | xargs)
 fi
