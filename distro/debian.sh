e645d3c783610bbb3b884cb83b8f0233eb0b7267
Lucas Campos Vieira <contato@lukerops.com>
2024-11-27 11:49:57 -0300

    distro: marca para não instalar os softwares recomendados no debian

--- distro/debian.sh
+++ distro/debian.sh
@@ -5,7 +5,7 @@ sudo apt update
 # sudo apt upgrade -y
 
 # instala o core do sistema
-sudo apt install $(cat ./distro/pkgs/*core.system.debian.pkgs | xargs)
+sudo apt install --no-install-suggests $(cat ./distro/pkgs/*core.system.debian.pkgs | xargs)
 
 # adiciona o usuário ao grupo de sudo
 # usermod -aG sudo $TARGET_USER
@@ -20,7 +20,7 @@ sudo sed -i 's/managed=false/managed=true/g' /etc/NetworkManager/NetworkManager.
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # instala os pacotes de audio
-sudo apt install $(cat ./distro/pkgs/pipewire.audio.debian.pkgs | xargs)
+sudo apt install --no-install-suggests $(cat ./distro/pkgs/pipewire.audio.debian.pkgs | xargs)
 
 # habilita o audio para o usuário
 systemctl --user daemon-reload
@@ -28,22 +28,22 @@ systemctl --user enable --now wireplumber.service pipewire.socket pipewire-pulse
 
 # instala o wifi se encontrar uma placa de rede
 if [[ $(lspci | egrep -i 'wifi|wireless') ]]; then
-    sudo apt install $(cat ./distro/pkgs/wireless.system.debian.pkgs | xargs)
+    sudo apt install --no-install-suggests $(cat ./distro/pkgs/wireless.system.debian.pkgs | xargs)
 fi
 
 # instala o bluetooth se encontrar uma dispositivo bluetooth
 if [[ $(lsusb | egrep -i 'bluetooth') ]]; then
-    sudo apt install $(cat ./distro/pkgs/bluetooth.system.debian.pkgs | xargs)
+    sudo apt install --no-install-suggests $(cat ./distro/pkgs/bluetooth.system.debian.pkgs | xargs)
 fi
 
 # instala o gnome se for o desktop escolhido
 if [[ $DESKTOP == 'gnome' ]]; then
-    sudo apt install $(cat ./distro/pkgs/gnome.desktop.debian.pkgs | xargs)
+    sudo apt install --no-install-suggests $(cat ./distro/pkgs/gnome.desktop.debian.pkgs | xargs)
 fi
 
 # instala o sway se for o desktop escolhido
 if [[ $DESKTOP == 'sway' ]]; then
-    sudo apt install $(cat ./distro/pkgs/sway.desktop.debian.pkgs | xargs)
+    sudo apt install --no-install-suggests $(cat ./distro/pkgs/sway.desktop.debian.pkgs | xargs)
 
     # cria link simbólico com as configurações
     mkdir -p $HOME/.config
