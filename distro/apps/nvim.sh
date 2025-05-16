968c46de545525025a02a6d2d9c1761b60f74c6e
Lucas Campos Vieira <contato@lukerops.com>
2025-05-15 21:04:23 -0300

    distro: ajusta script de instalação do neovim

--- distro/apps/nvim.sh
+++ distro/apps/nvim.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/bash
 
 # cria uma pasta temporária
-tmp=$(mktemp -d)
+TMPDIR=$(mktemp -d)
+NVIM_FILENAME='nvim-linux-x86_64'
 
 # Faz o donwload da versão estável mais recente do neovim
-wget --directory-prefix $tmp --relative 'https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz'
+wget --directory-prefix $TMPDIR --relative "https://github.com/neovim/neovim/releases/download/stable/${NVIM_FILENAME}.tar.gz"
 
 # Remove versão anterior do neovim
 sudo rm -rf /opt/nvim
 
 # Extraí o arquivo baixado na pasta /opt
-sudo tar --extract --gzip --directory=$tmp --file=$tmp/nvim-linux64.tar.gz
+tar --extract --gzip --directory=$TMPDIR --file=$TMPDIR/$NVIM_FILENAME.tar.gz
 
-# Move a pasta para /opt/nvim
-sudo mv $tmp/nvim-linux64 /opt/nvim
+# Renomeia a pasta para /opt/nvim
+sudo mv $TMPDIR/$NVIM_FILENAME /opt/nvim
 
 # Cria um link simbólico para o executável
 [[ ! -L /usr/local/bin/nvim ]] && sudo ln -s /opt/nvim/bin/nvim /usr/local/bin/
 
 # Remove a pasta temporária
-rm -rf $tmp
+rm -rf $TMPDIR
